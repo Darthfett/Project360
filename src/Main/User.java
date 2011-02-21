94939d9b17b28401ef7a33cb8fc77c6adddcb893
Darthfett <darthfett@gmail.com>
2011-02-21 14:35:35 -0700

    Fixed User.getUserLevel, Moved Users into Hashtable, added User.getUserFromUserName

--- src/Main/User.java
+++ src/Main/User.java
@@ -9,11 +9,20 @@ public class User {
 	//TODO: Make additional constructors that save the user.
 	private Hashtable<String, String> data;
 	private static File DataDir = new File("../Users");
-	private static ArrayList<User> Users = new ArrayList<User>();
+	private static Hashtable<String,User> Users = new Hashtable<String,User>();
 	
-	@SuppressWarnings("unchecked")
 	public static ArrayList<User> getUserList() {
-		return (ArrayList<User>) User.Users.clone();
+		ArrayList<User> userList = new ArrayList<User>();
+		User[] userListArr = (User[]) (User.Users.values().toArray(new User[0]));
+		for (int i = 0; i < userListArr.length; i++) {
+			userList.add(userListArr[i]);
+		}
+		
+		return userList;
+	}
+	
+	public static User getUserFromUserName(String username) {
+		return User.Users.get(username);
 	}
 
 	public String getUserName() {
@@ -57,14 +66,14 @@ public class User {
 	}
 
 	public UserLevel getUserLevel() {
-		String userLevelString = (String) data.get("userlevel");
-		if (userLevelString == "recruiter") {
+		String userLevelString = ((String) data.get("userlevel"));//.replace(" ","").replace("\n","");
+		if (userLevelString.equals("recruiter")) {
 			return UserLevel.RECRUITER;
-		} else if (userLevelString == "reviewer") {
+		} else if (userLevelString.equals("reviewer")) {
 			return UserLevel.REVIEWER;
-		} else if (userLevelString == "reference") {
+		} else if (userLevelString.equals("reference")) {
 			return UserLevel.REFERENCE;
-		} else if (userLevelString == "applicant") {
+		} else if (userLevelString.equals("applicant")) {
 			return UserLevel.APPLICANT;
 		} else {
 			System.out.println("DEBUG: Unknown user level: " + userLevelString);
@@ -72,9 +81,9 @@ public class User {
 		}
 	}
 	
-	public User(String username, String password, String userlevel) {
+	/*public User(String username, String password, String userlevel) {
 		
-	}
+	}*/
 
 	public User() {
 		//TODO: Replace this constructor with constructor that saves users
@@ -114,7 +123,6 @@ public class User {
 				String line = bufferedReader.readLine();
 				//Lines -> Arrays split by '='
 				User newUser = new User();
-				Users.add(newUser);
 				while (line != null) {
 					ArrayList<String> keyVal = new ArrayList<String>();
 					String[] temp = line.replace(" ", "").replace("\n", "").toLowerCase().split("=");
@@ -126,7 +134,7 @@ public class User {
 						keyVal.add("");
 					} else if (keyVal.size() == 2) {
 					} else {
-						System.out.print("WARNING: Invalid User File: " + userFiles[i] + " line: ");
+						System.out.print("WARNING: Invalid line in User File: " + userFiles[i]);
 						for(int j=0; j < keyVal.size();j++) {
 							System.out.print(keyVal.get(j) +",");
 						}
@@ -136,6 +144,12 @@ public class User {
 					line = bufferedReader.readLine();
 
 				}
+			if (newUser.data.containsKey("username")) {
+				User.Users.put(newUser.data.get("username"), newUser);
+			} else {
+				System.out.println("WARNING: User file " + userFiles[i] + " has no username");
+			}
+			
 			} catch (IOException ex) {
 				System.out.println("DEBUG: Unable to read from " + userFiles[i]);
 
